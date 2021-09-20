8ed4ba270065967083ac8b80f25cce7205ffd237
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-09-20 18:37:19 +0100

    Fixed the warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,19 @@ import { result } from "lodash";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };   
+  }, [term]);
+
 
   useEffect(() => {
     const search = async () => {
@@ -14,29 +26,16 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
-
-    if (term && !result.length) {
+    if (debouncedTerm) {
       search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-  
-      return () => {
-        clearTimeout(timeoutId);
-      };
     }
-
-    
-  }, [term]);
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
